634d4520130e8ba3d3abd08677f1f57440cbb547
dakesson-glitch <dakesson@browning.edu>
2026-02-20 14:17:26 -0500

    Update and rename test.py to Schedule.py and completed my schedule

---
+++ Schedule.py
@@ -0,0 +1,10 @@
+monday = "Monday: English, Specials, Snack, Research Principles B, Lunch and Office Hours,Chemistry B "
+tuesday = "Tuesday: Music, Chemistry, English, Lunch, Algebra, Latin, History"
+wednesday = "Wednesday: Computor Science, English, Latin, Lunch, Chemistry, Music"
+thursday = "Thursday: Chemistry, History, Algebra, Lunch, Research Principles, Community Time, Dialogue"
+friday = "Friday: Latin, Algebra, History, Lunch, Computor Science, Community Time, English"
+print(monday)
+print(tuesday)
+print(wednesday)
+print(thursday)
+print(friday)
