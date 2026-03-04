cffc01efe3c49e395c56ddbad3df9a5812908570
dakesson-glitch <dakesson@browning.edu>
2026-03-04 14:21:10 +0000

    I created a code to randomly pick a name from my class

---
+++ Name.py
@@ -0,0 +1,9 @@
+import random
+
+names = [
+    "David", "Nicholas", "Earl", "Ollie", "Alex", "Andrew",
+    "Peter", "Totti", "Ethan", "Devan", "Arjun", "Jerri"
+]
+
+chosen_name = random.choice(names)
+print(chosen_name)
